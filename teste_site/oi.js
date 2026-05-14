46e412e0503c9efa6ff8e80518995611057cc32e
Lab05_etec <Lab05_etec@etec>
2026-05-14 14:37:49 -0300

    ok

---
+++ teste_site/oi.js
@@ -0,0 +1,30 @@
+async function buscarUsers() {
+
+            
+
+    const rps = await fetch("https://api-9-mt0n.onrender.com/usuarios");
+
+    const usuarios = await rps.json();
+
+    const lista = document.getElementById("lista");
+
+    lista.innerHTML = "";
+
+    usuarios.forEach(usuario => {
+
+
+    lista.innerHTML += `
+        <li>
+
+        ${usuario.id}
+        ${usuario.nome}
+        ${usuario.telefone}
+        ${usuario.email}
+
+        
+        </li>
+    `;
+
+
+});
+}
